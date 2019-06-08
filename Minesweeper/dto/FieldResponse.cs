f63c642080f47353312c619ace271838892111d2
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-06-08 23:09:43 +0200

    Add missing comment

--- Minesweeper/dto/FieldResponse.cs
+++ Minesweeper/dto/FieldResponse.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 
 namespace Minesweeper.dto
 {
+    /// <summary>
+    /// Klasa reprezentująca odpowiedź dotyczącą pola planszy.
+    /// </summary>
     public class FieldResponse
     {
 
