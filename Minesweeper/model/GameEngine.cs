c61cd54c6a173fa0b6ba5bcfc9b87f0515c3e76c
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-06-11 18:01:18 +0200

    Add XML comments

--- Minesweeper/model/GameEngine.cs
+++ Minesweeper/model/GameEngine.cs
@@ -12,36 +12,86 @@ namespace Minesweeper.model
     /// </summary>
     internal class GameEngine
     {
+        #region Private fields
+
+        #endregion
+
         #region Properties
+
+        /// <summary>
+        /// Informacja o tym, czy gra została zakończona.
+        /// </summary>
         internal bool IsGameFinished { get { throw new NotImplementedException(); } }
+
+        /// <summary>
+        /// Informacja o tym, czy zakończona gra została wygrana.
+        /// </summary>
         internal bool IsResultPositive { get { throw new NotImplementedException(); } }
+
+        /// <summary>
+        /// Szerokość planszy.
+        /// </summary>
         internal int Width { get { throw new NotImplementedException(); } }
+
+        /// <summary>
+        /// Wysokość planszy.
+        /// </summary>
         internal int Height { get { throw new NotImplementedException(); } }
         #endregion
 
+        #region Methods
+
+        /// <summary>
+        /// Metoda rozpoczynająca nową grę.
+        /// </summary>
+        /// <param name="mode">Tryb nowej gry</param>
         internal void StartNewGame(GameModeEnum mode)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Metoda otwierająca wybrane pole.
+        /// </summary>
+        /// <param name="x">Współrzędna pozioma pola</param>
+        /// <param name="y">Współrzędna pionowa pola</param>
         internal void OpenField(int x, int y)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Metoda zaznaczająca / odznaczająca wybrane pole.
+        /// </summary>
+        /// <param name="x">Współrzędna pozioma pola</param>
+        /// <param name="y">Współrzędna pionowa pola</param>
         internal void MarkField(int x, int y)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Metoda uzyskująca wartość danego pola - sumę bomb na sąsiednich polach.
+        /// </summary>
+        /// <param name="x">Współrzędna pozioma pola</param>
+        /// <param name="y">Współrzędna pionowa pola</param>
+        /// <returns>Wartość pola</returns>
         internal int GetValue(int x, int y)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Metoda uzyskująca stan zaznaczenia danego pola.
+        /// </summary>
+        /// <param name="x">Współrzędna pozioma pola</param>
+        /// <param name="y">Współrzędna pionowa pola</param>
+        /// <returns>True w przypadku, gdy pole jest zaznaczone, false w przeciwnym przypadku</returns>
         internal bool GetMarked(int x, int y)
         {
             throw new NotImplementedException();
         }
+
+        #endregion
     }
 }
