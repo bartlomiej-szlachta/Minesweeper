7d691461efca942449832e49c601df4fd0b3421d
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-06-11 15:00:14 +0200

    Add app interface

---
+++ Minesweeper/view/InfoControl.Designer.cs
@@ -0,0 +1,73 @@
+﻿namespace Minesweeper.view
+{
+    partial class NumberControl
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Label = new System.Windows.Forms.Label();
+            this.Value = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // Label
+            // 
+            this.Label.AutoSize = true;
+            this.Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Label.Location = new System.Drawing.Point(3, 0);
+            this.Label.Name = "Label";
+            this.Label.Size = new System.Drawing.Size(80, 31);
+            this.Label.TabIndex = 0;
+            this.Label.Text = "Label";
+            // 
+            // Value
+            // 
+            this.Value.AutoSize = true;
+            this.Value.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.Value.Location = new System.Drawing.Point(100, 0);
+            this.Value.Name = "Value";
+            this.Value.Size = new System.Drawing.Size(83, 31);
+            this.Value.TabIndex = 1;
+            this.Value.Text = "Value";
+            // 
+            // NumberControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.Value);
+            this.Controls.Add(this.Label);
+            this.Name = "NumberControl";
+            this.Size = new System.Drawing.Size(186, 31);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label Label;
+        private System.Windows.Forms.Label Value;
+    }
+}
--- Minesweeper/view/MainForm.Designer.cs
+++ Minesweeper/view/MainForm.Designer.cs
@@ -28,13 +28,58 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numberControl2 = new Minesweeper.view.NumberControl();
+            this.numberControl1 = new Minesweeper.view.NumberControl();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 405);
+            this.panel1.TabIndex = 0;
+            // 
+            // numberControl2
+            // 
+            this.numberControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.numberControl2.Location = new System.Drawing.Point(602, 424);
+            this.numberControl2.Name = "numberControl2";
+            this.numberControl2.Size = new System.Drawing.Size(186, 31);
+            this.numberControl2.TabIndex = 6;
+            // 
+            // numberControl1
+            // 
+            this.numberControl1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.numberControl1.Location = new System.Drawing.Point(12, 424);
+            this.numberControl1.Name = "numberControl1";
+            this.numberControl1.Size = new System.Drawing.Size(186, 31);
+            this.numberControl1.TabIndex = 5;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 461);
+            this.Controls.Add(this.numberControl2);
+            this.Controls.Add(this.numberControl1);
+            this.Controls.Add(this.panel1);
+            this.MinimumSize = new System.Drawing.Size(500, 500);
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Minesweeoer";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private NumberControl numberControl1;
+        private NumberControl numberControl2;
     }
 }
 
