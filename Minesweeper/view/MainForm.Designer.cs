26cdc330b23a9bffdfad521da2c73b47c5176ace
Bartłomiej Szlachta <bszlachta1024@gmail.com>
2019-06-12 20:18:35 +0200

    Remove unnecessary InfoControl

--- Minesweeper/view/MainForm.Designer.cs
+++ Minesweeper/view/MainForm.Designer.cs
@@ -32,11 +32,10 @@
             this.menuStrip1 = new System.Windows.Forms.MenuStrip();
             this.gameToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.infoControl2 = new Minesweeper.view.InfoControl();
-            this.infoControl1 = new Minesweeper.view.InfoControl();
             this.beginnerToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.intermediateToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.expertToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.infoControl1 = new Minesweeper.view.InfoControl();
             this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -76,16 +75,29 @@
             this.intermediateToolStripMenuItem,
             this.expertToolStripMenuItem});
             this.newToolStripMenuItem.Name = "newToolStripMenuItem";
-            this.newToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(98, 22);
             this.newToolStripMenuItem.Text = "New";
             // 
-            // infoControl2
+            // beginnerToolStripMenuItem
             // 
-            this.infoControl2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.infoControl2.Location = new System.Drawing.Point(602, 424);
-            this.infoControl2.Name = "infoControl2";
-            this.infoControl2.Size = new System.Drawing.Size(186, 31);
-            this.infoControl2.TabIndex = 6;
+            this.beginnerToolStripMenuItem.Name = "beginnerToolStripMenuItem";
+            this.beginnerToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.beginnerToolStripMenuItem.Text = "Beginner";
+            this.beginnerToolStripMenuItem.Click += new System.EventHandler(this.beginnerToolStripMenuItem_Click);
+            // 
+            // intermediateToolStripMenuItem
+            // 
+            this.intermediateToolStripMenuItem.Name = "intermediateToolStripMenuItem";
+            this.intermediateToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.intermediateToolStripMenuItem.Text = "Intermediate";
+            this.intermediateToolStripMenuItem.Click += new System.EventHandler(this.intermediateToolStripMenuItem_Click);
+            // 
+            // expertToolStripMenuItem
+            // 
+            this.expertToolStripMenuItem.Name = "expertToolStripMenuItem";
+            this.expertToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.expertToolStripMenuItem.Text = "Expert";
+            this.expertToolStripMenuItem.Click += new System.EventHandler(this.expertToolStripMenuItem_Click);
             // 
             // infoControl1
             // 
@@ -95,32 +107,10 @@
             this.infoControl1.Size = new System.Drawing.Size(186, 31);
             this.infoControl1.TabIndex = 5;
             // 
-            // beginnerToolStripMenuItem
-            // 
-            this.beginnerToolStripMenuItem.Name = "beginnerToolStripMenuItem";
-            this.beginnerToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.beginnerToolStripMenuItem.Text = "Beginner";
-            this.beginnerToolStripMenuItem.Click += new System.EventHandler(this.beginnerToolStripMenuItem_Click);
-            // 
-            // intermediateToolStripMenuItem
-            // 
-            this.intermediateToolStripMenuItem.Name = "intermediateToolStripMenuItem";
-            this.intermediateToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.intermediateToolStripMenuItem.Text = "Intermediate";
-            this.intermediateToolStripMenuItem.Click += new System.EventHandler(this.intermediateToolStripMenuItem_Click);
-            // 
-            // expertToolStripMenuItem
-            // 
-            this.expertToolStripMenuItem.Name = "expertToolStripMenuItem";
-            this.expertToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
-            this.expertToolStripMenuItem.Text = "Expert";
-            this.expertToolStripMenuItem.Click += new System.EventHandler(this.expertToolStripMenuItem_Click);
-            // 
             // MainForm
             // 
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
             this.ClientSize = new System.Drawing.Size(800, 461);
-            this.Controls.Add(this.infoControl2);
             this.Controls.Add(this.infoControl1);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.menuStrip1);
@@ -139,7 +129,6 @@
 
         private System.Windows.Forms.Panel panel1;
         private InfoControl infoControl1;
-        private InfoControl infoControl2;
         private System.Windows.Forms.MenuStrip menuStrip1;
         private System.Windows.Forms.ToolStripMenuItem gameToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
